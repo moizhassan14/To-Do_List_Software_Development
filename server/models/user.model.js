d7e55219d19aeee76dbea13716ca53832ff56444
moiz hassan <118419901+moizhassan14@users.noreply.github.com>
2025-06-28 11:10:50 +0500

    docker files

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
